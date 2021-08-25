dd9f47e4a9766c54c9ad8ac4ce8a6a97aca9cb6e
Ramin <ramin.mousavy@gmail.com>
2021-08-25 21:39:45 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Konva from 'konva'
+
+export function App(options: any) {
+  const stage = new Konva.Stage(options)
+
+  return stage
+}
---
+++ tests/main.test.ts
@@ -0,0 +1,9 @@
+import { createEditor } from '../jest/utils/create-editor'
+
+describe('Shape -> Label', () => {
+  it('should create a triangle', () => {
+    const editor = createEditor()
+
+    expect(editor).toBeNull()
+  })
+})
